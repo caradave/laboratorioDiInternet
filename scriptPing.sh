f75d6c7295f20cb111a37ad57c51ceff33768f9a
caradave <davidecaramagna5@gmail.com>
2024-04-16 10:08:35 +0200

    update

---
+++ scriptPing.sh
@@ -0,0 +1,34 @@
+ #!/bin/bash
+
+touch misura.dat
+echo "" > misure.dat
+
+ for S in `seq 0 50 1472`
+ do
+ 	minRtt=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2 | cut -d "/" -f 1)
+ 	echo $S$minRtt>> misura.dat
+
+ done; 
+
+
+ for S in `seq 1490 150 3000`
+ do
+ 	minRtt=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2 | cut -d "/" -f 1)
+ 	echo $S$minRtt>> misura.dat
+
+ done; 
+
+ for S in `seq 3000 200 7000`
+ do
+ 	minRtt=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2 | cut -d "/" -f 1)
+ 	echo $S$minRtt>> misura.dat
+
+ done; 
+
+ for S in `seq 7000 300 10000`
+ do
+ 	minRtt=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2 | cut -d "/" -f 1)
+ 	echo $S$minRtt>> misura.dat
+
+ done; 
+
