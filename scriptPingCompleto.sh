1e12d47e1364fb96510ed7bffd95adb30a223793
caradave <davidecaramagna5@gmail.com>
2024-04-17 10:30:53 +0200

    update, grafici velocita TermToTerm, refactoring del codice

--- scriptPingCompleto.sh
+++ scriptPingCompleto.sh
@@ -1,32 +1,28 @@
- #!/bin/bash
+#!/bin/bash
 
- for S in `seq 1490 150 3000`;
- do
- 	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2);
- 	minRtt=$(echo $line ! cut -d "/" -f 1);
-	avgRtt=$(echo $line ! cut -d "/" -f 2);
-	maxRtt=$(echo $line ! cut -d "/" -f 3 | cut -d " " -f 1);
- 	echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
- done; 
+for S in `seq 1490 150 3000`;
+do
+	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2 | grep rtt | cut -d "=" -f 2);
+	minRtt=$(echo $line | cut -d "/" -f 1);
+	avgRtt=$(echo $line | cut -d "/" -f 2);
+	maxRtt=$(echo $line | cut -d "/" -f 3);
+	echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
+done; 
 
- for S in `seq 3000 200 7000`;
- do
- 	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2);
- 	minRtt=$(echo $line ! cut -d "/" -f 1);
-	avgRtt=$(echo $line ! cut -d "/" -f 2);
-	maxRtt=$(echo $line ! cut -d "/" -f 3 | cut -d " " -f 1);
- 	echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
- done; 
+for S in `seq 3000 200 7000`;
+do
+	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2 | grep rtt | cut -d "=" -f 2);
+	minRtt=$(echo $line | cut -d "/" -f 1);
+	avgRtt=$(echo $line | cut -d "/" -f 2);
+	maxRtt=$(echo $line | cut -d "/" -f 3);
+	echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
+done; 
 
- for S in `seq 7000 300 10000`;
- do
- 	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2| grep rtt | cut -d "=" -f 2);
- 	minRtt=$(echo $line ! cut -d "/" -f 1);
-	avgRtt=$(echo $line ! cut -d "/" -f 2);
-	maxRtt=$(echo $line ! cut -d "/" -f 3 | cut -d " " -f 1);
- 	echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
- done; 
- 
- gnuplot < grafico.sh
- 
-	
+for S in `seq 7000 300 10000`;
+do
+	line=$(ping 10.0.5.1 -c 50 -s $S -i 0,2 | grep rtt | cut -d "=" -f 2);
+	minRtt=$(echo $line | cut -d "/" -f 1);
+    avgRtt=$(echo $line | cut -d "/" -f 2);
+    maxRtt=$(echo $line | cut -d "/" -f 3);
+    echo $S" "$minRtt" "$avgRtt" "$maxRtt >> misura.dat;
+done;
